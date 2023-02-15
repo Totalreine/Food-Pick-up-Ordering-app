edf29b1b52338e67a8524c515112ebb2d00c8b9b
sudhasarathi <sudhav158@email.com>
2023-02-15 10:02:26 -0700

    created jquery for ajax method for login and signup form

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,17 +1,58 @@
 // Client facing scripts here
-$(() => {
-  $('#fetch-users').on('click', () => {
-    $.ajax({
-      method: 'GET',
-      url: '/api/users'
-    })
-    .done((response) => {
-      const $usersList = $('#users');
-      $usersList.empty();
+// jQuery for ajax method -login form
+$('#login-form').on('submit', function(e) {
+  e.preventDefault();
 
-      for(const user of response.users) {
-        $(`<li class="user">`).text(user.name).appendTo($usersList);
-      }
-    });
+  const email = $('#email-input').val();
+  const password = $('#password-input').val();
+
+  $.ajax({
+    url: '/login',
+    method: 'POST',
+    data: {
+      email: email,
+      password: password
+    },
+    success: function(response) {
+      window.location.href = '/';
+    },
+    error: function(xhr, status, error) {
+      $('#error-message').text(xhr.responseText);
+    }
+  });
+});
+
+// jQuery for ajax method -signup form
+$('#signup-form').on('submit', function(e) {
+  e.preventDefault();
+
+  const firstName = $('#first-name-input').val();
+  const lastName = $('#last-name-input').val();
+  const phoneNumber = $('#phone-number-input').val();
+  const email = $('#email-input').val();
+  const password = $('#password-input').val();
+  const city = $('#city-input').val();
+  const address = $('#address-input').val();
+  const postalCode = $('#postal-code-input').val();
+
+  $.ajax({
+    url: '/signup',
+    method: 'POST',
+    data: {
+      first_name: firstName,
+      last_name: lastName,
+      phoneNumber: phoneNumber,
+      email: email,
+      password: password,
+      city: city,
+      address: address,
+      postal_code: postalCode
+    },
+    success: function(response) {
+      window.location.href = '/';
+    },
+    error: function(xhr, status, error) {
+      $('#error-message').text(xhr.responseText);
+    }
   });
 });
