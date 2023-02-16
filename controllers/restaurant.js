d3de7833b447d1b548a179e7d3ead2a9e1fae5e1
Juan Escobar <jcarriola83@gmail.com>
2023-02-16 17:37:09 +0000

    routes refactoring and debbuging

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,137 +1,119 @@
 
+const { dirname } = require('path');
 const db = require('../db/connection');
-
-exports.getRestLogin = (req, res) => {
-
-    // check for restuarents email, otherwise redirect to login
-    if (!req.session.user_id || req.session.user_id !== 1) {
-      res.redirect("/login");
-      return;
-    }
-
-    // display restuarent page
-    const user = req.session.user_id;
-    const templateVars = { user };
-    res.render("restuarent", templateVars);
-  };
 
 exports.getDishes = (req, res) => {
 
   db.query(`SELECT * FROM dishes`)
   .then(data => {
-    const name = data.rows;
-    res.json({ name });
+    const dishes = data.rows;
+    res.json({ dishes });
+    res.send(dishes)
   })
   .catch(err => {
-    res
+    console.log(err)
+    /*res
       .status(500)
-      .json({ error: err.message });
+      .json({ error: err.message }); */
   });
 }
 
 exports.getDish = (req, res) => {
-
-  const id = parseInt(req.params.id);
+  const id = req.params.dishId
+   
     db.query(`SELECT * FROM dishes WHERE id = $1`,[id])
       .then(data => {
-        const customers = data.rows;
-        res.json({ customers });
+        const dish = data.rows[0].name;
+        
+        res.json({ dish });
+        res.send(dish)
       })
       .catch(err => {
-        res
+        console.log(err)
+      /*  res
           .status(500)
-          .json({ error: err.message });
+          .json({ error: err.message }); */
       });
 }
 
 exports.getAddDish = (req, res) => {
-  const id = parseInt(req.params.id);
-    db.query(`INSERT INTO dishes (restaurant_id, name, description, price, food_category, vegan, gluten_free, picture_url, rating)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9
-    `,[id])
-      .then(data => {
-        const customers = data.rows;
-        res.json({ customers });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-        });
+  res.render("addDishtest")
 }
 
 exports.getEditDish = (req, res) => {
-  const id = parseInt(req.params.id);
-    db.query(`UPDATE dishes
-    SET name = $1, description = $2, food_category = $3, vegan = $4, gluten_free = $, picture_url = $6, 
-    price = $7, rating = $8, 
-    WHERE id = $9;`,[id])
+  const id = req.params.dishId
+    db.query(`SELECT * FROM dishes WHERE id = $1`,[id])
       .then(data => {
-        const customers = data.rows;
-        res.json({ customers });
+        const dish = data.rows[0];
+       
+        res.render("editDishtest", {dish})
       })
       .catch(err => {
-        res
+        console.log(err)
+        /*es
           .status(500)
-          .json({ error: err.message });
-        });
+          .json({ error: err.message }); */
+      });
 
 }
 
 exports.postAddDish = (req, res) => {
-  const { restaurant_id, name, description, price, food_category, vegan, gluten_free, picture_url, rating} = req.body;
-    console.log(req.body);
+  const {name, description, price, food_category, vegan, gluten_free, picture_url, rating} = req.body;
+   
     db.query(
-             `INSERT INTO dishes (restaurant_id, name, description, price, food_category, vegan, gluten_free, picture_url, rating ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-      [restaurant_id, name, description, price, food_category, vegan, gluten_free, picture_url, rating]
+             `INSERT INTO dishes ( name, description, price, 
+              food_category, vegan, gluten_free, picture_url, rating ) 
+              VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+      [name, description, price, food_category, vegan, gluten_free, picture_url, rating]
     )
       .then(data => {
-        const carts = data;
-        res.json({ carts });
+        const dish = data.rows[0];
+        //res.json({ dish });
+        const id = dish.id
+        res.redirect(`/dishes/${id}`)
       })
       .catch(err => {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({ ERROR: err.message });
       });
 }
 
 exports.postEditDish = (req, res) => {
-
-  const { restaurant_id, name, description, price, food_category, vegan, gluten_free, picture_url, rating} = req.body;
-    console.log(req.body);
+  
+  const {id, name, description, price, food_category, vegan, gluten_free, picture_url, rating} = req.body;
+    
        db.query(
-          `UPDATE dishes SET restaurant_id = $1, name = $2, description = $3, price = $4, food_category = $5, vegan = $6, gluten_free = $7, picture_url = $8, rating = $9 WHERE id = $10 RETURNING *`,
-         [ restaurant_id, name, description, price, food_category, vegan, gluten_free, picture_url, rating]
+          `UPDATE dishes SET name = $1, description = $2, price = $3, food_category = $4, vegan = $5, gluten_free = $6, picture_url = $7, rating = $8 WHERE id = $9 RETURNING *`,
+         [name, description, price, food_category, vegan, gluten_free, picture_url, rating, id]
     )
       .then(data => {
-        const carts = data;
-        res.json({ carts });
+        const dish = data.rows[0];
+        res.json({ data });
+        res.send(dish)
       })
       .catch(err => {
-        res.status(500).json({ error: err.message });
+        console.log(err)
+        //res.status(500).json({ error: err.message });
       });
 }
 
 exports.deleteDish = (req, res) => {
-
-    const { id } = req.params;
-    console.log(req.params);
+    const id = req.body.id;
+    
     db.query(
       `DELETE FROM dishes WHERE id = $1 RETURNING *`,
       [id]
     )
       .then(data => {
-        if (data.rowCount > 0) {
           const deletedDish = data.rows[0];
-          res.json({ message: `Dish ${deletedDish.name} has been deleted.` });
-        } else {
-          res.status(404).json({ error: `Dish with ID ${id} not found.` });
-        }
+          res.redirect("/dishes")
       })
       .catch(err => {
-        res.status(500).json({ error: err.message });
+        console.log(err)
+        //res.status(500).json({ error: err.message });
       });
   };
 
+  
   
   
   
